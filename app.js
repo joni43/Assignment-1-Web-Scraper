93629f2dcc2f2bf577b33296ce04dc5d6ddd9b4e
jonathan <jl224dq@student.lnu.se>
2018-01-30 22:53:59 +0100

    Change my request. Using promise-request module due i missed having promises on my latest code

--- app.js
+++ app.js
@@ -10,129 +10,119 @@
 const fs = require('fs')
 const cheerio = require('cheerio')
 const request = require('request')
+var rp = require('request-promise')
 
 const url = ('http://vhost3.lnu.se:20080/weekend')
 
-function WebcrawlUrl (callback) {
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
+function WebcrawlUrl () {
+  /. Number 1, Skapar var options, som tar url, och hämtar,laddar boddy från url enligt cheerio
+  var options = {
+    url: url,
+    transform: function (body) {
+      return cheerio.load(body)
     }
+  }
+  // Number 1.requstar url och laddar body. Skriver enligt syntaxen i dokumentationen.
+  rp(options).then(function ($) {
     let StartUrl = []
-    let $ = cheerio.load(body)
-    links = $('a')
-    $(links).each(function (i, link) {
-     // console.log(link) //Ger ett konstigt terminal utskrift
+    $('a').each(function (i, link) {
       let AllUrl = $(this).attr('href')
       StartUrl.push(AllUrl) // pushar in alla url i en tom array
     })
     let CalendarLink = StartUrl[0] // tar första url, Calendar. och sedan requestar den.
-    let CinemaLink = StartUrl[1] /7 // Tar andra url, cinema,
+    let CinemaLink = StartUrl[1] / 7 // Tar andra url, cinema,
     let ResturantLink = StartUrl[2] // tar ut tredje url, resturang
 
-
-    request(CalendarLink, function (error, response, body) {
-      if (!error && response.statusCode === 200) {
+    // Requstar första url, och får ut sidan med 3 länkar till Cinema,Calender och resturang. Tar ut dessa länkar geom ta varje 'a' och letar attr 'href'
+// ---------------------------------------------------------------------------------------
+// Rquest CALENDER. Number 2
+    var options = {
+      url: CalendarLink,
+      transform: function (body) {
+        return cheerio.load(body)
       }
+    }
+    //Number 2
+    rp(options).then(function ($) {
       let usersURL = []
-      let $$ = cheerio.load(body)
-      let links2 = $$('a')
 
-      $(links2).each(function (i, link) {
-      var page_link = $$(this).attr('href')
-      usersURL.push(page_link)
-    })
+      $('a').each(function (i, link) {
+        var page_link = $(this).attr('href')
+        usersURL.push(page_link)
+      }) // Get links from paul peter mary. 35-42.Correct
 
-      let similarDates = []
-      usersURL.forEach(function (link) {
-      link = CalendarLink + link
-      request(link, function (error, response, body) {
-            if (!error && response.statusCode === 200) {
+      let finalData = []
+      // do a for loop for usersURL array
+      for (let i = 0; i < usersURL.length; i++) {
+         // Number 3
+        var options = {
+          url: CalendarLink + usersURL[i],
+          transform: function (body) {
+            return cheerio.load(body)
+          } // Getting cheerio boady for peter paul nad mary.
+        }
+        rp(options).then(function ($) { // number 3
+          var tdData = []
+          $('tbody tr td').each(function (d) {
+            tdData.push($(this).text().toLowerCase())
+          })
+          finalData.push(tdData)
+          console.log(finalData)
+          if (usersURL.length === finalData.length) {
+            console.log(finalData)
+            var days = ['Friday', 'Saturday', 'Sunday']
+
+            if (finalData.length > 0) {
+              for (i = 0; i < finalData.length; i++) {
+                if (finalData[0][i] && finalData[1][i] && finalData[2][i]) {
+                  console.log(`On ${days[i]}, we found a match for you guys at index: ${i}`)
+                  break
+                }
+              }
             }
-
-        var $ = cheerio.load(body)
-        var user_dates = []
-        var tr = $('tr')
-        tr.each(function () {
-          $(this).find('td').each(function () {
-              user_dates.push($(this).text().toLowerCase())   // get ok message
-            })
-          similarDates.push(user_dates)
-          if (usersURL.length === similarDates.length) {
           }
+        }).catch(function (err) { // 3.  Catchar peter.html,paul och marry err.
+          console.log(err)
         })
+      }
+    }).catch(function (err) { // 2. catchar CALENDER ERR
+      console.log(err)
+    })
+  }).catch(function (err) { // 1. Catchar StartURL ERR
+    console.log(err)
+  })
 
-        similarDates.push(user_dates)
-        let FridayisOk = user_dates[0]
-        let AllOk = []
-        AllOk.push(FridayisOk)
+      // usersURL.forEach(function (link) {
+      //   link = CalendarLink + link
+      //   request(link, function (error, response, body) {
+      //     if (!error && response.statusCode === 200) {
+      //   }
 
-      //   for (var i in peopeOK){
-      //         if(peopeOK.indexOf('ok', i) > -1) {
-      //               avaibledas.push(peopeOK.indexof('ok',i))
+      //     var $ = cheerio.load(body)
+      //     var user_dates = []
+      //     var tr = $('tr')
+      //     tr.each(function () {
+      //     $(this).find('td').each(function () {
+      //       user_dates.push($(this).text().toLowerCase())   // get ok message
+      //     })
+      //     console.log(user_dates)
+      //     similarDates.push(user_dates)
+
+      //     if (usersURL.length === similarDates.length) {
+      //       console.log(similarDates)
+      //     }
+      //   })
+      //     var days = ['Friday', 'Saturday', 'Sunday']
+
+      //     if (similarDates.length < 0) {
+      //     for (let i = 0; i < similarDates.length; i++) {
+      //           if (similarDates[0][i] && similarDates[1][i] && similarDates[2][i]) {
+      //                 console.log(`On ${days[i]}, we found a match for you guys at index: ${i}`)
+      //                 break
+      //               }
       //         }
       //   }
-        // console.log('Friday is ' + AllOk)
-        // ta reda på vilken position ok finns i arrayen. alla 3.
-
-            // var days = ['Friday', 'Saturday', 'Sunday']
-
-            // if (similarDates.length < 0) {
-            //       for (i = 0; i < similarDates.length; i++){
-            //            if (similarDates[0][i] && similarDates[1][i] && similarDates[2][i]) {
-            //             break
-            //            }
-            //       }
-
-            // }
-      })
-    })
-    })
-
-    callback(error, StartUrl)
-  })
 }
 WebcrawlUrl(function (err, links) {
   if (err) return console.log(err)
 })
-// const fs = require('fs')
-// // const cheerio = require('cheerio')
-// const request = require('request')
-// const { JSDOM } = require('jsdom')
-
-// const url = ('http://vhost3.lnu.se:20080/weekend')
-
-// function WebcrawlUrl (url) {
-//   request.get(url, function (error, response, html) {
-//      if (!error && response.statusCode === 200) {
-//        const dom = new JSDOM(html)
-//        Array.from(dom.window.document.querySelectorAll('a')).forEach(element => {
-//         console.log(element.href)
-//           // consol loge calend,cinema å dinner.
-//          WebcrawlUrl(element.href) // Call function in a function, går nu in i en av href element. calendar)
-
-//        })
-//      }
-//    })
-//  }
-//  WebcrawlUrl(url)
-
-// Call function in a function.Recursion.
-// const url = ('http://vhost3.lnu.se:20080/weekend')
-// url + /calendar +
-
-            // get ok message
-            // user_dates.push($(this).text())
-
-            // switch (i) {
-            //   case 0:
-            // weekend.friday = $(this).html()
-
-      // case 1:
-            // weekend.saturday = $(this).html()
-
-            // break
-            // case 2:
-            // weekend.sunday = $(this).html()
-
-            // }
-            // console.log(weekend)
