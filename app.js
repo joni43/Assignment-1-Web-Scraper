5ccca59e0773e29866101b90b5e30c4d50aa30ad
jonathan <jl224dq@student.lnu.se>
2018-02-11 17:42:36 +0100

    Get movieObject from cinema

--- app.js
+++ app.js
@@ -7,30 +7,30 @@
 'user strict'
 // var pageLoader = require('./js/pageLoader')
 const cheerio = require('cheerio')
-// const request = require('request')
 var rp = require('request-promise')
 const getLinks = require('./js/calender')
 
 const url = process.argv[2] || ('http://vhost3.lnu.se:20080/weekend')
 
 let homelinks = []
+let freedays = []
 
 async function WebCrawl (url) {
   let links = await getLinks.getLinks(url)
   return links
 }
-
 let startlinks = WebCrawl(url)
 
-startlinks.then(function (StartUrl) {
+startlinks.then(async function (StartUrl) {
   homelinks = StartUrl
   let calenderUrl = homelinks[0]
-  let usersCal = getLinks.Calendar(calenderUrl)
-  // console.log(usersCal)
-  return usersCal
+  let cinemaUrl = homelinks[1]
+  let resturantUrl = homelinks[2]
 
-  // CalendarLink = StartUrl[0]
-}).then(function (usersCal){
+  let usersCal = await getLinks.Calendar(calenderUrl) // Här la jag till await
   console.log(usersCal)
+  let cinemaCal = await getLinks.Cinema(cinemaUrl)  // Och här
+  console.log(cinemaCal)
+  let test = await getLinks.tryingHard()
+  console.log(test)
 })
-// .then(function () {
--- js/calender.js
+++ js/calender.js
@@ -1,7 +1,9 @@
 'use strict'
-
+let request = require('request')
 const cheerio = require('cheerio')
 const rp = require('request-promise')
+var bodyParser = require('body-parser')
+const fetch = require('node-fetch')
 let availableDays = []
 
 function fetchCheerio (url) {
@@ -24,53 +26,104 @@ function getLinks (url) {
 }
 
 // Number 2
+/**
+ * Read the calendar page html page and retrieve the ok and sync day
+ * @param calUrl
+ */
 
 function Calendar (calUrl) {
-  return new Promise(function(resolve, reject) {
-  fetchCheerio(calUrl).then(function ($) {
-    let usersURL = []
-    $('a').each(function (i, link) {
-      var pageLink = $(link).attr('href')
-      usersURL.push(pageLink)
-    }) // Get links from paul peter mary. 35-42.Correct
+  return new Promise(function (resolve, reject) {
+    fetchCheerio(calUrl).then(function ($) {
+      let usersURL = []
+      $('a').each(function (i, link) {
+        var pageLink = $(link).attr('href')
+        usersURL.push(pageLink)
+      }) // Get links from paul peter mary. 35-42.Correct
 
-    let finalData = []
+      let finalData = []
         // do a for loop for usersURL array
-    for (let i = 0; i < usersURL.length; i++) {
+      for (let i = 0; i < usersURL.length; i++) {
         // Number 3
-      var options = {
-        url: calUrl + usersURL[i],
-        transform: function (body) {
-          return cheerio.load(body)
-        } // Getting cheerio boady for peter paul nad mary.
-      }
-      rp(options).then(function ($) { // number 3
-        var tdData = []
-        $('tbody tr td').each(function (d) {
-          tdData.push($(this).text().toLowerCase())
-        })
-        finalData.push(tdData)
-        if (usersURL.length === finalData.length) {
-          var days = ['Friday', 'Saturday', 'Sunday']
-
-          if (finalData[0][0] && finalData[1][0] && finalData[2][0] === 'ok') {
-            availableDays.push('05')
-              console.log('its friday')
+        var options = {
+          url: calUrl + usersURL[i],
+          transform: function (body) {
+            return cheerio.load(body)
+          } // Getting cheerio boady for peter paul nad mary.
+        }
+        rp(options).then(function ($) { // number 3
+          var tdData = []
+          $('tbody tr td').each(function (d) {
+            tdData.push($(this).text().toLowerCase())
+          })
+          finalData.push(tdData)
+          if (usersURL.length === finalData.length) {
+            if (finalData[0][0] && finalData[1][0] && finalData[2][0] === 'ok') {
+              availableDays.push('05')
+           //   console.log('its friday')
             } else if (finalData[0][1] && finalData[1][1] && finalData[2][1] === 'ok') {
               availableDays.push('06')
-              console.log('its Saturday')
+              // console.log('its Saturday')
             } else if (finalData[0][2] && finalData[1][2] && finalData[2][2] === 'ok') {
-              console.log('its Sunday')
+              availableDays.push('07')
+              // console.log('its Sunday')
             }
-          resolve(availableDays)
-        }
-      }).catch(function (err) { // 3.  Catchar peter.html,paul och marry err.
+            resolve(availableDays)
+          }
+        }).catch(function (err) { // 3.  Catchar peter.html,paul och marry err.
           console.log(err)
         })
+      }
+    })
+  })
+}
+/**
+ * Read the cinema page and retrieve the movies
+ * @param CinUrl
+ */
+function Cinema (CinUrl) {
+  let moviesList = []
+  return new Promise(function (resolve, reject) {
+    fetchCheerio(CinUrl).then(function ($) {
+      $('option').filter(function () {
+        let movie = $(this).attr('value')
+        // {let test = ($(this).text())
+       //  console.log(test)} Detta consologar day och movie
+        if (movie === '01' || movie === '02' || movie === '03') {
+          moviesList.push($(this).text())
+        }
+        // console.log(moviesList)
+      })
+      resolve(moviesList)
+    }).catch(function (err) {
+      console.log(err)
+    })
+  })
+}
+function tryingHard () {
+  return new Promise(function (resolve, reject) {
+    let day = ''
+    let movie = ''
+    let MovieObjects = []
+    for (let j = 5; j <= 7; j += 1) {
+      day = '0' + j
+
+      for (let i = 1; i <= 3; i += 1) {
+        movie = '0' + i
+
+        fetch('http://vhost3.lnu.se:20080/cinema/check?day=' + day + '&movie=' + movie)
+      .then(res => res.text())
+      .then(body => {
+        MovieObjects.push(body)
+        if (MovieObjects.length === 9) {
+          resolve(MovieObjects)
+        }
+      })
+      }
     }
   })
-})
 }
 
 module.exports.getLinks = getLinks
 module.exports.Calendar = Calendar
+module.exports.Cinema = Cinema
+module.exports.tryingHard = tryingHard
